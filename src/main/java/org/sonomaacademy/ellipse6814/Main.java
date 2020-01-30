48e6d77e528622a429e1a660058217220ea711fb
ivy-brenner <59379342+ivy-brenner@users.noreply.github.com>
2020-01-29 16:03:37 -0800

    new main.java

--- src/main/java/org/sonomaacademy/ellipse6814/Main.java
+++ src/main/java/org/sonomaacademy/ellipse6814/Main.java
@@ -6,7 +6,11 @@ class Main {
 
     public static void main(String[] args){
         System.out.println("Hello World!");
-        Direction d = Direction.NORTH;
-        System.out.println(d);
+        int x = 10;
+        int y = 10;
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
     }
 }

c2b7c17b64609067825469e39b3ca7f98d72d9ad
quinthemint <59623248+quinthemint@users.noreply.github.com>
2020-01-29 16:06:53 -0800

    Replacing old main with mine

--- src/main/java/org/sonomaacademy/ellipse6814/SimpleApp.java
+++ src/main/java/org/sonomaacademy/ellipse6814/SimpleApp.java
@@ -5,8 +5,15 @@ import org.sonomaacademy.ellipse6814.Direction;
 class Main {
 
     public static void main(String[] args){
+        int a = 7 + 7;
+        int b = 7 - 7;
+        int c = 7 * 7;
         System.out.println("Hello World!");
         Direction d = Direction.NORTH;
         System.out.println(d);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
